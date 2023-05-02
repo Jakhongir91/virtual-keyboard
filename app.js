1c4d7281a9b47a30c1c4bb3681c681c498710c5c
Jakhongir <jakhongir.nishanbaev@gmail.com>
2023-05-02 10:59:24 +0500

    feat: highlight virtual key on keyboard press

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const inputField = document.querySelector(".textarea__input");
 const keys = document.querySelectorAll(".key");
 
 keys.forEach((key) => {
+    /*let keyValue = key.textContent.trim();
+    let values = [];
+    let secondary = null;
+    let primary = null;
+
+    keyValue = keyValue.replace(/  +/g, ' ').toLowerCase();
+    console.log("Value: " + keyValue);
+    values = keyValue.split(' ');
+    console.log("Length: " + values.length);
+
+    if (values.length > 1) {
+        [secondary, primary] = values;
+    } else {
+        [primary] = values;
+    }
+
+    key.dataset.primary = primary;
+    key.dataset.secondary = secondary;*/
+
     key.addEventListener('click', (event) => {
         if (event.eventPhase === 2) {
             event.stopPropagation();
@@ -19,3 +38,31 @@ keys.forEach((key) => {
         inputField.textContent += keyPressed;
     })
 })
+
+document.addEventListener('keydown', (event) => {
+    const keyPrimary = document.querySelector(`[data-primary="${event.key}"], [data-primary="${event.key.toLowerCase()}"]`);
+    const keySecondary = document.querySelector(`[data-secondary="${event.key}"]`);
+    console.log(event.key);
+
+    if (keyPrimary) {
+        keyPrimary.classList.add("key__pressed");
+    }
+
+    if (keySecondary) {
+        keySecondary.classList.add("key__pressed");
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    const keyPrimary = document.querySelector(`[data-primary="${event.key}"], [data-primary="${event.key.toLowerCase()}"]`);
+    const keySecondary = document.querySelector(`[data-secondary="${event.key}"]`);
+    console.log(event.key);
+
+    if (keyPrimary) {
+        keyPrimary.classList.remove("key__pressed");
+    }
+
+    if (keySecondary) {
+        keySecondary.classList.remove("key__pressed");
+    }
+});
